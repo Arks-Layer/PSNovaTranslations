b0d89eaafc2fbd906b637a29635cfed130c7e0f7
Alam Arias <Alam.GBC@gmail.com>
2016-09-25 14:11:38 -0400

    travis: check json files

---
+++ checkjson.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+import re
+import os
+import simplejson
+import sys
+
+invalid_json_files = []
+json_files = [f for f in os.listdir('.') if re.match(r'.*\.json', f)]
+for files in json_files:
+    with open(files) as json_file:
+        try:
+            l = []
+            l.append('{')
+            l.append(json_file.read()[2:-2])
+            l.append('}')
+	    s = ''.join(l)
+            simplejson.loads(s)
+        except ValueError as e:
+            print("%s: %s") % (files, e)
+            invalid_json_files.append(files)
+
+count = len(invalid_json_files)
+if count != 0:
+    sys.exit("=============\nJSON files with issues: %d" % count)
