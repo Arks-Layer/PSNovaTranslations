5fc08b859f17cadc39555d865a91e32c89637c5e
Marrub <marrub--@users.noreply.github.com>
2016-11-01 13:54:40 -0400

    gen_enemy_mats: Process cores, make ProcFile
    enemy_cores: Fix Small Core and add Bird Core

--- gen_enemy_mats.py
+++ gen_enemy_mats.py
@@ -43,11 +43,44 @@ def LoadEnemies(fname):
    j   = json.loads("{" + open(fname, "r").read()[:-2] + "}")
 
    for code in j:
+   #{
       obj = j[code]
       if "Text" in obj and "Enabled" in obj:
          ret[obj["OriginalText"]] = obj["Text"]
+   #}
 
    return ret
+#}
+
+##
+## ProcFile
+##
+def ProcFile(fp, out, enemies, dictionary):
+#{
+   for ln in fp:
+   #{
+      match = re.match("\\s+\"OriginalText\": \"(.+)の(.+)\",\r\n", ln)
+
+      if match is not None and         \
+         match.group(1) in enemies and \
+         match.group(2) in dictionary:
+      #{
+         ln1 = next(fp)
+
+         if re.search("\"Text\": \"..+\"", ln1) is None and \
+            re.search("\"Enabled\"", ln1)       is None:
+         #{
+            ln = ln + \
+                 "    \"Text\": \"" + enemies[match.group(1)] + \
+                 " " + dictionary[match.group(2)] + "\",\r\n" + \
+                 "    \"Enabled\": true\r\n"
+         #}
+         else:
+            ln = ln + ln1
+      #}
+
+      out.write(ln)
+   #}
 #}
 
 ##
@@ -55,46 +88,25 @@ def LoadEnemies(fname):
 ##
 def Main():
 #{
-   dictionary = {
-      "甲殻": "Shell",
-      "目片": "Eye",
-      "眼片": "Eye",
-      "刃片": "Blade",
-      "脚片": "Leg",
-      "鱗片": "Scale",
-      "殻片": "Husk",
-      "皮片": "Pelt"
-   }
-
    enemies = LoadEnemies("enemies.json")
 
-   with open("misc_items.json", "r", newline="") as fp:
-   #{
-      for ln in fp:
-      #{
-         match = re.match("\\s+\"OriginalText\": \"(.+)の(.+)\",\r\n", ln)
+   with open("misc_items.json", "r", newline = "") as fp:
+      with open("misc_items.json.1", "w", newline = "") as out:
+         ProcFile(fp, out, enemies,
+         {
+            "甲殻": "Shell",
+            "目片": "Eye",
+            "眼片": "Eye",
+            "刃片": "Blade",
+            "脚片": "Leg",
+            "鱗片": "Scale",
+            "殻片": "Husk",
+            "皮片": "Pelt"
+         })
 
-         if match is not None and         \
-            match.group(1) in enemies and \
-            match.group(2) in dictionary:
-         #{
-            ln1 = next(fp)
-
-            if re.search("\"Text\": \"..+\"", ln1) is None and \
-               re.search("\"Enabled\"", ln1)      is None:
-            #{
-               ln = ln + \
-                    "    \"Text\": \"" + enemies[match.group(1)] + \
-                    " " + dictionary[match.group(2)] + "\",\r\n" + \
-                    "    \"Enabled\": true\r\n"
-            #}
-            else:
-               ln = ln + ln1
-         #}
-
-         sys.stdout.write(ln)
-      #}
-   #}
+   with open("enemy_cores.json", "r", newline = "") as fp:
+      with open("enemy_cores.json.1", "w", newline = "") as out:
+         ProcFile(fp, out, enemies, { "・コア": "Core" })
 #}
 
 
