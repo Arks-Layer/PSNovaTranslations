320f31cc8a273075277516202f987db677f83cda
Alam Arias <Alam.GBC@gmail.com>
2016-11-07 20:25:01 -0500

    checkjson: add check for Text and Enabled

--- checkjson.py
+++ checkjson.py
@@ -9,7 +9,10 @@ json_files = [f for f in os.listdir('.') if re.match(r'.*\.json', f)]
 for files in json_files:
     with open(files) as json_file:
         try:
-            simplejson.load(json_file)
+            djson = simplejson.load(json_file)
+            for rmid in djson:
+                if (not ("Text" in djson[rmid]) or (djson[rmid]["Text"] != "")) and (not ("Enabled" in djson[rmid]) or (djson[rmid]["Enabled"] == False)):
+                    print ("%s:%s" % (files, rmid))
         except ValueError as e:
             print("%s: %s") % (files, e)
             invalid_json_files.append(files)
